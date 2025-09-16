f2ff9d655203da309e0812467c755dadc6139663
Spencer <salane2232@gmail.com>
2025-09-15 20:42:53 -0400

    Commit Annual Maintenance

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -13,7 +13,15 @@ public class SalesTax {
             System.out.println();
             System.out.println();
 
+        System.out.println( "Task 2 Annual Maintenance Cost");
+        double summerCost = 64;
+        double springCost = 79;
+        double autumnCost = 46;
+        double winterCost = 35;
+        double totalCost = 0; //placeholder input for year
 
+        totalCost = springCost + summerCost + autumnCost + winterCost;
+        System.out.println( "The total annual maintenance cost is $" + totalCost);
 
     }
 }
