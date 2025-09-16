09715a4622e85c829ad8bf17a1f1131924874cb8
Spencer <salane2232@gmail.com>
2025-09-15 21:11:52 -0400

    First Commit

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -13,29 +13,5 @@ public class SalesTax {
             System.out.println();
             System.out.println();
 
-        System.out.println( "Task 2 Annual Maintenance Cost");
-        double summerCost = 64;
-        double springCost = 79;
-        double autumnCost = 46;
-        double winterCost = 35;
-        double totalCost = 0; //placeholder input for year
-
-        totalCost = springCost + summerCost + autumnCost + winterCost;
-        System.out.println( "The total annual maintenance cost is $" + totalCost);
-        System.out.println( "Monthly CC Balance"); //Task 3
-        double balance = 5000.0;
-        double interestRate = 0.17;
-        double firstMonth = 0;
-        double secondMonth = 0;
-
-        firstMonth = balance + (balance * interestRate);
-        secondMonth = firstMonth + (firstMonth * interestRate);
-
-        System.out.println( " At the end of the first month the balance equals $" + firstMonth);
-        System.out.println( " At the end of the second month the balance equals $" + secondMonth);
-
-        System.out.println();
-        System.out.println();
-
     }
 }
